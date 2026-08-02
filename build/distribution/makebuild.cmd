a654add27e218993b428a79c90c754694e479a29
Howard Kapustein <howardk@microsoft.com>
2026-08-02 08:07:03 -0700

    Working on msix.exe support

---
+++ build/distribution/makebuild.cmd
@@ -0,0 +1,34 @@
+@ECHO Off
+SETLOCAL
+
+IF %1x == x GoTo Help
+IF %2x == x GoTo Help
+
+SET CONFIG=%1
+SET ARCH=%2
+
+SET OUTDIR=%~dp0..\..\bin\%CONFIG%\%ARCH%\msixadmin
+SET BINDIR_MSIX=%OUTDIR%\..\msix
+
+CALL C:\Util\vc26vars64.bat
+
+msbuild -p:Configuration=%CONFIG%;Platform=%ARCH% -t:Rebuild -p:MSIXADMIN=0 MSIXcli.sln
+IF ERRORLEVEL 1 GoTo TheEnd
+IF NOT EXIST %BINDIR_MSIX% MD %BINDIR_MSIX%
+ECHO COPY %OUTDIR%\msixadmin.exe %BINDIR_MSIX%\msix.exe
+COPY %OUTDIR%\msixadmin.exe %BINDIR_MSIX%\msix.exe
+COPY %OUTDIR%\msixadmin.pdb %BINDIR_MSIX%\msix.pdb
+ECHO COPY %OUTDIR%\msixadmin.pdb %BINDIR_MSIX%\msix.pdb
+
+msbuild -p:Configuration=%CONFIG%;Platform=%ARCH% -t:Rebuild -p:MSIXADMIN=1 MSIXcli.sln
+IF ERRORLEVEL 1 GoTo TheEnd
+
+GoTo TheEnd
+
+:Help
+ECHO Usage: MAKEBUILD configuration architecture
+ECHO   configuration = Debug or Release
+ECHO    architecture = x64 OR arm64
+
+:TheEnd
+ENDLOCAL
